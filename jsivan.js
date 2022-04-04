59215ec91b1990c09a7a7f4fa5510f13456a7203
Bekzhan <bekzhanzulkashev@gmail.com>
2022-04-04 19:43:31 +0500

    first commit

---
+++ jsivan.js
@@ -0,0 +1,31 @@
+// // const category = 'toys';
+
+// // console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Beka';
+// alert(`Hi, ${user}`);
+
+console.log( 'arr' + " - object"); 
+console.log(4 + "5");
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 == '6');
+
+&& - оператор И
+|| - оператор или
+
+const isChecked = false,
+     isClose = true;
+
+console.log(isChecked || !isClose);
